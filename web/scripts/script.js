eb5968c2b9c3c0d18729f723f01a5b21b5c775af
Michael Schindler <madsn@gmx.de>
2015-08-22 16:40:36 +0200

    Add javascript

---
+++ web/scripts/script.js
@@ -0,0 +1,35 @@
+$( function() {
+    var options = {
+            lines: { show: true, lineWidth: 10 },
+            points: { show: true, radius: 5 },
+            xaxis: {
+                mode: "time",
+                font: { size: 20 },
+                color: '#000000'
+            },
+            yaxis: {
+                font: { size: 20 },
+                color: '#000000'
+            }
+        },
+        ajaxRequestStatus = false;
+
+    function ajaxCall() {
+        if ( ajaxRequestStatus === true ) {
+            return;
+        }
+
+        ajaxRequestStatus = true;
+
+        $.get( "/api" + window.location.pathname, function( data ) {
+            $.plot( "#chart", data, options );
+        } ).always( function() {
+            ajaxRequestStatus = false;
+        } );
+    }
+
+    setTimeout( ajaxCall, 60000 );
+
+    ajaxCall();
+} );
+
