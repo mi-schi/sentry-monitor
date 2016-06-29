279106e397f49331120369ce121248b35464973a
Michael Schindler <madsn@gmx.de>
2016-06-29 13:58:11 +0200

    Add event migration service

---
+++ src/Migration/EventTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace EnlightenedDC\Sentry\Monitor\Migration;
+
+use Doctrine\DBAL\Connection;
+
+/**
+ * @package EnlightenedDC\Sentry\Monitor\Migration
+ */
+class EventTable
+{
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @param Connection $connection
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     */
+    public function create()
+    {
+        $createTableSql = 'CREATE TABLE IF NOT EXISTS events (
+            organisation TEXT,
+            project TEXT,
+            event_id INTEGER,
+            created TEXT,
+            UNIQUE(organisation, project, event_id, created)
+        )';
+
+        $this->connection->executeQuery($createTableSql);
+    }
+}
